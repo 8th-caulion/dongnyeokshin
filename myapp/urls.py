e20fc8950db68e4e7a7bc63bf0de84903f71eb47
dn.shin98@gmail.com <dn.shin98@gmail.com>
2020-04-25 10:51:43 +0900

    complete adding detail page except U&D

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import path
 import myapp.views
 
+
 urlpatterns = [
     path('', myapp.views.main, name="main"),
     path('profile/', myapp.views.profile, name="profile"),
+    path('blog/<int:blog_id>', myapp.views.detail, name="detail"),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Blog
+from django.utils import timezone
 
 # Create your views here.
 def main(request):
@@ -8,3 +9,7 @@ def main(request):
 
 def profile(request):
     return render(request, 'profile.html')
+
+def detail(request, blog_id):
+    details = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'detail.html', {'details':details})
